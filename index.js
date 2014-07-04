7ade2d5eedfa47a27a3fa7fdd5e50372dcf9da89
lily-zhangying <zhangying3210@gmail.com>
2014-07-04 11:24:51 +0800

    add yog-log

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 var macpack = require('mcpack');
 var nshead = require('nshead');
-var wise = 'http://tc-wise-adaptdb00.tc.baidu.com:7777';
 var provider = 'Mildx';
-var request = require('request');
-var http = require('http');
 var getInstance = require('./lib/socket.js').getInstance;
+var log;
 // var url = '';
 
 
-http.createServer(function(req, res){
+// http.createServer(function(req, res){
     
-    // console.log(req);
-    //把req组装发到
+//     // console.log(req);
+//     //把req组装发到
     
-    var socket = getInstance();
-    socket.get("", "", "")
-    .then(function(data){
-        console.log(data);
-    })
-    .fail(function(err){
-        console.log(err);
-    });
+//     var socket = getInstance();
+//     socket.get("", "", "")
+//     .then(function(data){
+//         console.log(data);
+//     })
+//     .fail(function(err){
+//         console.log(err);
+//     });
 
 
 
-}).listen(8878);
+// }).listen(8878);
+
+module.exports = function(server, provider){
+    console.log(server);
+    console.log(provider);
+
+    return function(req, res, next){
+        var socket = getInstance();
+        socket.get("", "", "")
+        .then(function(data){
+            console.log(data);
+            res.wise = data;
+        }) 
+        .fail(function(err){
+            console.log(err);
+        })
+        .done(function(){
+            console.log('done');
+            next();
+        });
+    }
+}
+
+
